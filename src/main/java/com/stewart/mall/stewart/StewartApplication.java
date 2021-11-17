705c0706c166f382e47e1fd83a6c0b7b1b165434
Stewart <sukura13123133927@qq.com>
2021-11-17 19:18:02 +0800

    init

---
+++ src/main/java/com/stewart/mall/stewart/StewartApplication.java
@@ -0,0 +1,13 @@
+package com.stewart.mall.stewart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StewartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stewart/mall/stewart/StewartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stewart.mall.stewart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
